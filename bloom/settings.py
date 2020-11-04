4c57c346e106b0bb1468e94b612db18dcb3e9ea9
mybloomwork <68631267+mybloomwork@users.noreply.github.com>
2020-11-05 00:48:28 +0100

    Update settings.py

--- bloom/settings.py
+++ bloom/settings.py
@@ -37,7 +37,7 @@ SECRET_KEY = 'q9e4j-gd*ji($g&bqhu1u$acc=#3+^bb=+$t8$&gt!kj+=1tjo'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['134.209.72.206']
+ALLOWED_HOSTS = ['159.65.237.36']
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
